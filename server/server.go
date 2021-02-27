54a9dc3284926a18030104fe59c8bac49b55dd1f
Janghyub Seo <seojanghyeob@gmail.com>
2021-02-27 13:04:34 +0900

    Move server.go

---
+++ server/server.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"fmt"
+	"net"
+)
+
+func Server(adr string) {
+	listener, err := net.Listen("tcp", adr)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("The BlockBase Server has started to %v", adr)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if nil != err {
+			fmt.Printf("\nConnection Error: %v", err)
+			continue
+		} else {
+			fmt.Printf("\nConnected from: %v\n", conn.RemoteAddr())
+		}
+		go func() {
+			buffer := make([]byte, 3000)
+			stringA := ""
+			conn.Read(buffer)
+			for _, data := range buffer {
+				if data != 0 {
+					stringA = stringA + string(data)
+				}
+			}
+			if err == nil {
+				fmt.Print(stringA)
+			}
+		}()
+	}
+
+}
